93e9f57d54f7ec91fe03e7a5e7e934d3e3e415e2
druide <ingvuil@gmail.com>
2015-11-18 17:37:55 +0200

    [fix] undefined res.writeHead on closed connection [refactor] standard style

--- proxy.js
+++ proxy.js
@@ -1,94 +1,92 @@
-"use strict";
+'use strict'
 
-var debug_log = require('debug')('scluster:log');
-debug_log.log = console.log.bind(console);
-var debug_error = require('debug')('scluster:error');
+var debug_log = require('debug')('scluster:log')
+debug_log.log = console.log.bind(console)
 
+var httpProxy = require('http-proxy')
+var http = require('http')
 
-var httpProxy = require('http-proxy');
-var http = require('http');
+var current_proxy = 0
+var total_workers = 0
 
-var current_proxy = 0;
-var total_workers = 0;
+function next_proxy () {
+  var proxy = proxies[current_proxy]
 
-function next_proxy() {
-	var proxy = proxies[current_proxy];
-	
-	require('util').inspect(proxies, false, null);
-	
-	current_proxy = (current_proxy + 1) % total_workers;
-	return proxy;
+  require('util').inspect(proxies, false, null)
+
+  current_proxy = (current_proxy + 1) % total_workers
+  return proxy
 }
 
+var stickers = {}
 
-var stickers = {};
+var proxies = {}
 
-var proxies = {};
+exports.init = function (workers, first_port, proxy_port, session_hash, no_sockets) {
+  total_workers = workers
 
+  for (var n = 0; n < total_workers; n++) {
+    proxies[n] = httpProxy.createProxyServer({
+      target: {
+        host: '127.0.0.1',
+        port: first_port + n
+      }
+    })
 
-exports.init = function(workers, first_port, proxy_port, session_hash, no_sockets) {
+    proxies[n].on('error', function (error, req, res) {
+      var json
+      debug_log('proxy error: ' + error)
+      // connection may be already closed and res.writeHead will be undefined
+      if (!res.headersSent && typeof res.writeHead === 'function') {
+        res.writeHead(500, {'content-type': 'application/json'})
+      }
 
-	total_workers = workers;
-	
-	for (var n = 0; n < total_workers; n++) {
-		proxies[n] = new httpProxy.createProxyServer({
-			target : {
-				host : '127.0.0.1',
-				port : first_port + n
-			}
-		});
-	}
+      json = { error: 'proxy_error', reason: error.message }
+      res.end(JSON.stringify(json))
+    })
+  }
 
-	var server = http.createServer(function(req, res) {
-		get_proxy(session_hash, req, res).web(req, res);
-	});
+  var server = http.createServer(function (req, res) {
+    get_proxy(session_hash, req, res).web(req, res)
+  })
 
-	if (!no_sockets)
-	{
-		server.on('upgrade', function(req, socket, head) {
-			get_proxy(session_hash, req).ws(req, socket, head);
-	
-		});
-	}
+  if (!no_sockets) {
+    server.on('upgrade', function (req, socket, head) {
+      get_proxy(session_hash, req).ws(req, socket, head)
+    })
+  }
 
-	debug_log("main proxy listen on port: " + proxy_port);
-	
-	server.listen(proxy_port);
+  debug_log('main proxy listen on port: ' + proxy_port)
+
+  server.listen(proxy_port)
 }
 
+function get_proxy (session_hash, req, res) {
+  var hash = session_hash(req, res)
 
-function get_proxy(session_hash, req, res)
-{
-	var hash = session_hash(req, res);
+  debug_log('hash: ' + hash)
 
-	debug_log('hash: ' + hash);
-	
-	var proxy = undefined;
+  var proxy
 
-	if (hash !== undefined) {
+  if (hash !== undefined) {
+    if (stickers[hash] !== undefined) {
+      debug_log('restored proxy.')
 
-		if (stickers[hash] !== undefined) {
+      proxy = stickers[hash].proxy
+    } else {
+      debug_log('assigned proxy.')
 
-			debug_log('restored proxy.');
+      proxy = next_proxy()
 
-			proxy = stickers[hash].proxy;
-		} else {
+      stickers[hash] = {
+        proxy: proxy
+      }
+    }
+  } else {
+    debug_log('random proxy.')
 
-			debug_log('assigned proxy.');
-			
-			proxy = next_proxy();
+    proxy = next_proxy()
+  }
 
-			stickers[hash] = {
-				proxy : proxy,
-			}
-		}
-
-	} else {
-
-		debug_log('random proxy.');
-
-		proxy = next_proxy();
-	}
-
-	return proxy;
+  return proxy
 }
